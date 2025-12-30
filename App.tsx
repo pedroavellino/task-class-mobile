3529d6455165ab8d962eae159094f98a0ffbcae8
Pedro Avelino <pedro_avelino_@hotmail.com>
2025-12-30 08:36:34 -0300

    feat: initial mobile project setup

---
+++ App.tsx
@@ -0,0 +1,15 @@
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Task Class Mobile ✅</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: { flex: 1, alignItems: "center", justifyContent: "center" },
+});
